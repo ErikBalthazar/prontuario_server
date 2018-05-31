b2fcbc06822bb2c19ded2b2f01af8d9d361b97d9
ErikBalthazar <erik.balthazar@gmail.com>
2018-05-30 23:42:55 -0300

    first blood

---
+++ front-to-test/client/src/App.js
@@ -0,0 +1,157 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: ""
+    };
+  }
+
+  clickName() {
+    let name = document.getElementById("name_text");
+    axios.post("http://localhost:8000/test", {
+      name: name.value
+    })
+    .then((response) => {
+      console.log("deu bom");
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.log("deu ruim: ", error);
+    });
+  }
+
+  signIn() {
+    let login = document.getElementById("login_text");
+    let pass = document.getElementById("pass_text");
+
+    axios.post("http://localhost:8000/login/paciente", {
+      login: login.value,
+      pass: pass.value
+    })
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  facebook() {
+    let name = document.getElementById("name");
+    let email = document.getElementById("email");
+    let birthdate = document.getElementById("birthdate");
+    let fbid = document.getElementById("fbid");
+
+    axios.post("http://localhost:8000/paciente/facebook/new", {
+      name: name.value,
+      email: email.value,
+      birthdate: birthdate.value,
+      fbId: fbid.value
+    })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });    
+  }
+
+  // TESTES DIRETO NO BOTAO
+  addPacient() {
+    axios.post("http://localhost:8000/paciente/new", {
+      cpf: "11111111111",
+      email: "teste@teste.com",
+      name: "Testando",
+      birthdate: "2002-05-11",
+      password: "123",
+      phoneNumber: "1933231132",
+      gender: "M"
+    })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    }); 
+  } 
+  
+  addDoctor() {
+    axios.post("http://localhost:8000/medico/new", {
+      crm: "11111",
+      email: "teste@teste.com",
+      name: "Abobrinha",
+      birthdate: "2002-05-11",
+      password: "123",
+      gender: "M",
+      phoneNumber: "1934561234"
+    })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    }); 
+  }
+
+  addDoctorWorkLocation() {
+    axios.post("http://localhost:8000/medico/maps/add", {
+      crm: "11111",
+      maps: "UniMetrocamp Campinas"
+    })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    }); 
+  }
+  
+  addDoctorSignin() {
+    axios.post("http://localhost:8000/login/medico", {
+      login: "11111",
+      pass: "123"
+    })
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    }); 
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div>
+          <input type="text" id="name_text"/>
+          <input type="button" value="Clica aqui" onClick={() => this.clickName()}/>
+        </div>
+        <div>
+          <input type="text" id="login_text" placeholder="Login"/>
+          <input type="password" id="pass_text" placeholder="Senha"/>
+          <input type="button" value="Clica aqui" onClick={() => this.signIn()}/>
+        </div>
+        <div>
+          <input type="text" id="name" placeholder="Nome"/>
+          <input type="text" id="email" placeholder="Email"/>
+          <input type="text" id="birthdate" placeholder="Data"/>
+          <input type="text" id="fbid" placeholder="FBID"/>
+          <input type="button" value="Clica aqui" onClick={() => this.facebook()}/>
+        </div>
+        <div>
+          <input type="button" value="Cadastra paciente" onClick={() => this.addPacient()}/>
+          <input type="button" value="Cadastra médico" onClick={() => this.addDoctor()}/>
+          <input type="button" value="Adicionar local de trabalho do médico" 
+            onClick={() => this.addDoctorWorkLocation()}/>
+          <input type="button" value="Login médico" onClick={() => this.addDoctorSignin()}/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,436 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var mysql      = require('mysql');
+var app = express()
+
+const BAD_REQUEST = 400;
+const SUCCESS = 200;
+const NOT_FOUND = 404;
+const SERVER_ERROR = 500;
+
+const connectionData = {
+    host     : 'localhost',
+    port     : 3306,
+    user     : 'root',
+    password : '',
+    database : 'prontuario_beta'
+};
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next();
+});
+
+app.get('/', function(req, res) {
+    res.send('TA FUNCIONANDO!')
+});
+
+app.post('/test', function(req, res) {
+    let name = req.body.name
+    res.json({notes: `Hello ${name}`})
+});
+
+// AGORA É SÉRIO
+
+app.post('/login/paciente', function(req, res) {
+    let cpf = req.body.login;
+    let pass = req.body.pass;
+
+    let query = `SELECT * FROM paciente WHERE cpf = '${cpf}' AND senha = '${pass}'`;
+
+    const connection = mysql.createConnection(connectionData);
+
+    connection.connect(function(err) {
+        if (err) throw err;
+        connection.query(query, function(errors, results, fields) {
+            if (errors) throw errors;
+            console.log(results);
+            if (results.length > 0) {
+                let data = results[0];
+                res.json(
+                    {
+                        cpf: data.CPF,
+                        email: data.EMAIL,
+                        name: data.NOME_COMPLETO,
+                        birthdate: data.DATA_NASCIMENTO,
+                        password: data.SENHA,
+                        phoneNumber: data.TELEFONE,
+                        gender: data.SEXO,
+                        fbid: data.FBID
+                    });
+            } else {
+                res.sendStatus(BAD_REQUEST);
+            }
+        });
+    });
+}); 
+
+app.post('/login/medico', function(req, res) {
+    let crm = req.body.login;
+    let pass = req.body.pass;
+
+    let query = `SELECT * FROM medico WHERE crm = '${crm}' AND senha = '${pass}'`;
+
+    const connection = mysql.createConnection(connectionData);
+
+    connection.connect(function(err) {
+        if (err) throw err;
+        connection.query(query, function(errors, results, fields) {
+            if (errors) throw errors;
+            console.log(results);
+            if (results.length > 0) {
+                let data = results[0];
+                res.json(
+                    {
+                        crm: data.CRM,
+                        email: data.EMAIL,
+                        name: data.NOME_COMPLETO,
+                        birthdate: data.DATA_NASCIMENTO,
+                        password: data.SENHA,
+                        phoneNumber: data.TELEFONE,
+                        gender: data.SEXO,
+                        maps: data.MAPS
+                    });
+            } else {
+                res.sendStatus(BAD_REQUEST);
+            }
+        });
+    });
+}); 
+
+app.post('/paciente/new', function(req, res) {
+    let data = req.body;
+    let cpf = data.cpf;
+    //let bloodType = data.bloodType; // nao esta no form
+    let email = data.email;
+    let name = data.name;
+    let birthdate = data.birthdate;
+    let password = data.password;
+    let phoneNumber = data.phoneNumber;
+    let gender = data.gender;
+
+    let query = `INSERT INTO paciente (CPF, EMAIL, NOME_COMPLETO, DATA_NASCIMENTO, SENHA, TELEFONE, SEXO) ` +
+        `VALUES ('${cpf}', '${email}', '${name}', '${birthdate}', '${password}', '${phoneNumber}', '${gender}')`;
+
+    const connection = mysql.createConnection(connectionData);
+    
+    connection.connect(function(err) {
+        if (err) throw err;
+        connection.query(query, function(errors, results, fields) {
+            if (errors) throw errors;
+            console.log(results);
+            res.json({ msg: "Paciente cadastrado com sucesso" });
+        });
+    });
+});
+
+app.post('/paciente/update', function(req, res) {
+    let data = req.body;
+    let cpf = data.cpf;
+    let bloodType = data.bloodType; // nao esta no form
+    let email = data.email;
+    let name = data.name;
+    let birthdate = data.birthdate;
+    let password = data.password;
+    let phoneNumber = data.phoneNumber;
+    let gender = data.gender;
+
+    let query = `UPDATE paciente SET EMAIL = '${email}', NOME_COMPLETO = '${name}', ` + 
+        `DATA_NASCIMENTO = '${birthdate}', SENHA = '${password}', ` + 
+        `TELEFONE = '${phoneNumber}', SEXO = '${gender}' WHERE CPF = '${cpf}'`;
+
+    const connection = mysql.createConnection(connectionData);
+    
+    connection.connect(function(err) {
+        if (err) throw err;
+        connection.query(query, function(errors, results, fields) {
+            if (errors) throw errors;
+            console.log(results);
+            res.json({ msg: "Paciente atualizado com sucesso" });
+        });
+    });
+});
+
+app.post('/medico/new', function(req, res) {
+    let data = req.body;
+    let crm = data.crm;
+    let email = data.email;
+    let name = data.name;
+    let birthdate = data.birthdate;
+    let password = data.password;
+    let phoneNumber = data.phoneNumber;
+    let gender = data.gender;
+
+    let query = `INSERT INTO medico (CRM, EMAIL, NOME_COMPLETO, DATA_NASCIMENTO, SENHA, SEXO, TELEFONE) ` +
+    `VALUES ('${crm}', '${email}', '${name}', '${birthdate}', '${password}', '${gender}', '${phoneNumber}')`;
+
+    const connection = mysql.createConnection(connectionData);
+    
+    connection.connect(function(err) {
+        if (err) throw err;
+        connection.query(query, function(errors, results, fields) {
+            if (errors) throw errors;
+            console.log(results);
+            res.json({ msg: "Medico cadastrado com sucesso" });
+        });
+    });
+});
+
+app.post('/medico/update', function(req, res) {
+    let data = req.body;
+    let crm = data.crm;
+    let email = data.email;
+    let name = data.name;
+    let birthdate = data.birthdate;
+    let password = data.password;
+    let phoneNumber = data.phoneNumber;
+    let gender = data.gender;
+
+    let query = `UPDATE medico SET EMAIL = '${email}', NOME_COMPLETO = '${name}', ` + 
+    `DATA_NASCIMENTO = '${birthdate}', SENHA = '${password}', ` + 
+    `SEXO = '${gender}' TELEFONE = ${phoneNumber} WHERE CRM = '${crm}'`;
+
+    const connection = mysql.createConnection(connectionData);
+    
+    connection.connect(function(err) {
+        if (err) throw err;
+        connection.query(query, function(errors, results, fields) {
+            if (errors) throw errors;
+            console.log(results);
+            res.json({ msg: "Medico atualizado com sucesso" });
+        });
+    });
+});
+
+
+app.post('/paciente/facebook/new', function (req, res) {
+    let data = req.body;
+    let email = data.email;
+    let name = data.name;
+    let birthdate = data.birthdate;
+    let fbid = data.fbId;
+
+    console.log(data);
+
+    let query = `SELECT * from paciente WHERE fbid = ${fbid}`;
+   
+    const connection = mysql.createConnection(connectionData);
+    
+    connection.connect(function(err) {
+        if (err) throw err;
+        connection.query(query, function(errors, results, fields) {
+            if (errors) throw errors;
+            console.log(results);
+            if (results.length > 0) {
+                res.json({ msg: "Logado com sucesso" });
+            } else {
+                query = `INSERT INTO paciente (email, nome_completo, data_nascimento, fbid) VALUES ('${email}', '${name}', '${birthdate}', '${fbid}')`;
+                connection.query(query, function(errors2, results2, fields2) {
+                    if (errors2) throw errors2;
+                    console.log(results2);
+                    res.json({ msg: "Logado com sucesso" });
+                });
+            }
+        });
+    });
+});
+
+app.post('/paciente/cpf', function(req, res){
+    let cpf = req.body.cpf;
+
+    let query = `SELECT * FROM paciente WHERE cpf = '${cpf}'`;
+
+    const connection = mysql.createConnection(connectionData);
+
+    connection.connect(function(err) {
+        if (err) throw err;
+        connection.query(query, function(errors, results, fields) {
+            if (errors) throw errors;
+            console.log(results);
+            if (results.length > 0) {
+                let data = results[0];
+                res.json(
+                    {
+                        cpf: data.CPF,
+                        email: data.EMAIL,
+                        name: data.NOME_COMPLETO,
+                        birthdate: data.DATA_NASCIMENTO,
+                        password: data.SENHA,
+                        phoneNumber: data.TELEFONE,
+                        gender: data.SEXO,
+                        fbid: data.FBID
+                    });
+            } else {
+                res.sendStatus(BAD_REQUEST);
+            }
+        });
+    });  
+});
+
+app.post('/paciente/fbid', function(req, res){
+    let fbid = req.body.fbid;
+
+    let query = `SELECT * FROM paciente WHERE fbid = '${fbid}'`;
+
+    const connection = mysql.createConnection(connectionData);
+
+    connection.connect(function(err) {
+        if (err) throw err;
+        connection.query(query, function(errors, results, fields) {
+            if (errors) throw errors;
+            console.log(results);
+            if (results.length > 0) {
+                let jsonResult = {
+                    cpf: results[0]
+                };
+                res.json(jsonResult);
+            } else {
+                res.sendStatus(BAD_REQUEST);
+            }
+        });
+    });  
+});
+
+app.post('/paciente/email', function(req, res){
+    let email = req.body.email;
+
+    let query = `SELECT * FROM paciente WHERE email = '${email}'`;
+
+    const connection = mysql.createConnection(connectionData);
+
+    connection.connect(function(err) {
+        if (err) throw err;
+        connection.query(query, function(errors, results, fields) {
+            if (errors) throw errors;
+            console.log(results);
+            if (results.length > 0) {
+                let jsonResult = {
+                    cpf: results[0]
+                };
+                res.json(jsonResult);
+            } else {
+                res.sendStatus(BAD_REQUEST);
+            }
+        });
+    });  
+});
+
+app.post('/medico/crm', function(req, res){
+    console.log(req)
+
+    let crm = req.body.crm;
+
+    let query = `SELECT * FROM medico WHERE crm = '${crm}'`;
+
+    const connection = mysql.createConnection(connectionData);
+
+    connection.connect(function(err) {
+        if (err) throw err;
+        connection.query(query, function(errors, results, fields) {
+            if (errors) throw errors;
+            console.log(results);
+            if (results.length > 0) {
+                let data = results[0];
+                res.json(
+                    {
+                        crm: data.CRM,
+                        email: data.EMAIL,
+                        name: data.NOME_COMPLETO,
+                        birthdate: data.DATA_NASCIMENTO,
+                        password: data.SENHA,
+                        phoneNumber: data.TELEFONE,
+                        gender: data.SEXO,
+                        maps: data.MAPS
+                    });
+            } else {
+                res.sendStatus(BAD_REQUEST);
+            }
+        });
+    });  
+});
+
+app.post('/paciente/facebook/add', function (req, res) {
+    let cpf = req.body.cpf;
+    let fbid = req.body.fbid;
+
+    let query = `SELECT * FROM paciente WHERE cpf = '${cpf}'`;
+
+    const connection = mysql.createConnection(connectionData);  
+    
+    connection.connect(function(err) {
+        if (err) throw err;
+        connection.query(query, function(errors, results, fields) {
+            if (errors) throw errors;
+            console.log(results);
+            if (results.length > 0) {
+                query = `UPDATE paciente SET fbid = ${fbid} WHERE cpf = '${cpf}'`;
+                res.json({ msg: "Facebook adicionado ao perfil"});
+            } else {
+                res.sendStatus(BAD_REQUEST);
+            }
+        });
+    });  
+});
+
+app.post('/medico/maps/add', function (req, res) {
+    let crm = req.body.crm;
+    let maps = req.body.maps;
+
+    let query = `SELECT * FROM medico WHERE crm = '${crm}'`;
+
+    const connection = mysql.createConnection(connectionData);  
+    
+    connection.connect(function(err) {
+        if (err) throw err;
+        connection.query(query, function(errors, results, fields) {
+            if (errors) throw errors;
+            console.log(results);
+            if (results.length > 0) {
+                query = `UPDATE medico SET maps = '${maps}' WHERE crm = '${crm}'`;
+                connection.query(query, function(errors2, results2, fields2) {
+                    if (errors2) throw errors2;
+                    res.json({ maps: maps});
+                });
+            } else {
+                res.sendStatus(BAD_REQUEST);
+            }
+        });
+    });  
+});
+
+/*
+// FUNCIONANDO
+app.post('/paciente/facebook/new', function (req, res) {
+    let data = req.body;
+    let email = data.email;
+    let name = data.name;
+    let birthdate = data.birthdate;
+    let fbid = data.fbId;
+
+    console.log(data);
+
+    let query = `INSERT INTO paciente (email, nome_completo, data_nascimento, fbid) VALUES ('${email}', '${name}', '${birthdate}', '${fbid}')`;
+   
+    const connection = mysql.createConnection(connectionData);
+    
+    connection.connect(function(err) {
+        if (err) throw err;
+        connection.query(query, function(errors, results, fields) {
+            if (errors) throw errors;
+            console.log(results);
+            res.json({ msg: "Logado com sucesso" });
+            //if (results.length > 0) {
+            //    res.send(results);
+            //} else {
+            //    res.sendStatus(BAD_REQUEST);
+            //}
+        });
+    });
+});
+*/
+
+app.listen(8000)
+
